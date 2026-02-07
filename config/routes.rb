ce45e96d54c6f3ae5b6d9e5915a48e91dbad8433
nihad1213 <nihad.nemetli@gmail.com>
2026-02-07 15:11:47 +0400

    upd: all routes writed as a single way

--- config/routes.rb
+++ config/routes.rb
@@ -12,16 +12,18 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get "/products", to: "products#index"
+  # get "/products", to: "products#index"
   
-  get "/products/new", to: "products#new"
-  post "/products", to: "products#create"
+  # get "/products/new", to: "products#new"
+  # post "/products", to: "products#create"
   
-  get "/products/:id", to: "products#show"
+  # get "/products/:id", to: "products#show"
 
-  get "/products/:id/edit", to: "products#edit"
-  patch "/products/:id", to: "products#update"
-  put "/products/:id", to: "products#update"
+  # get "/products/:id/edit", to: "products#edit"
+  # patch "/products/:id", to: "products#update"
+  # put "/products/:id", to: "products#update"
 
-  delete "/products/:id", to: "products#destroy"
+  # delete "/products/:id", to: "products#destroy"
+
+  resources :products
 end
